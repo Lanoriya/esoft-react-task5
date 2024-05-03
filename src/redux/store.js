a19b6eb2f7dc6dab642a38aa923b8ea2ffd540b0
lanoriya <Mr_Buzik@mail.ru>
2024-05-03 15:08:55 +0500

    watch later/favorites

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import filmReducer from './filmSlice';
 import filterReducer from './filterSlice';
+import filmFavoriteReducer from './filmFavorite';
+import filmWatchLaterReducer from './filmWatchLater';
 
 export default configureStore({
   reducer: {
     films: filmReducer,
     filter: filterReducer,
+    filmFavorite: filmFavoriteReducer,
+    filmLater: filmWatchLaterReducer, 
   },
 })
