2e78a044fda2c6db7f6ef1c92a3accabebb4904a
lanoriya <Mr_Buzik@mail.ru>
2024-05-06 11:17:48 +0500

    router fix

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import 'tailwindcss/tailwind.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename={'/esoft-react-task5/'}>
         <App />
       </Router>
     </Provider>
